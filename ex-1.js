59aa2195e0c8fa91ff7afc69bea86be4154574ac
Wanarak <wanarak.r@gmail.com>
2024-03-27 02:14:15 +0700

    Assignment: Type Conversion

--- ex-1.js
+++ ex-1.js
@@ -1,6 +1,9 @@
 let accountBalance = 400;
 let depositAmountFromUser = "10";
 
+Number(depositAmountFromUser);
+depositAmountFromUser = Number(depositAmountFromUser);
+
 accountBalance = accountBalance + depositAmountFromUser;
 
 let accountBalanceMessage = "Your account balance is " + accountBalance;
